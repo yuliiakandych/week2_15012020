3e5b4e42b03a30a481ab02c272d2f8c54ffdb2e2
Yuliia Kandych <yuliiakandych@YuliiaD13825593.fanshawec.ca>
2020-01-15 09:45:47 -0500

    naav

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
+
+var vm = new Vue({
+  el: "#app",
+
+  data: {
+
+    // mock up the user - this well eventually come from the database UMS (user management system)
+    user: {
+      isAdmin: false,
+      avatar: 'thor.png',
+      isLoggedIn: true
+    },
+    
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false
+  },
+
+  methods: {
+    setUserPref(){
+      //this is a preferences control, hit the api when ready (or use the component)
+      console.log('set user pref here');
+    },
+
+    userLogin() {
+      //call the login rote , or load the login component
+      console.log('do login / logout on click');
+
+      this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    }
+  }
+});
